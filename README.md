dac6ce229bc1166e8575cf8bd05996f4a26e5488
Tom Liu <liumaizi@gmail.com>
2026-05-11 22:15:33 -0700

    Initial Commit. Set up project

---
+++ README.md
@@ -0,0 +1,9 @@
+### LLM's Solve Math Problems
+Authors: Henry Songyang Wang and Tom Liu
+
+## Instructions to use our code
+1. source .venv/bin/activate
+2. pip install -r requirements.txt
+
+
+## LLM's Used
---
+++ src/test.py
@@ -0,0 +1,19 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+client = OpenAI(
+    base_url="https://openrouter.ai/api/v1",
+    api_key=os.environ["OPENROUTER_API_KEY"],
+)
+
+response = client.chat.completions.create(
+    model="baidu/cobuddy:free",
+    messages=[
+        {"role": "user", "content": "Hello!"}
+    ],
+)
+
+print(response.choices[0].message.content)
